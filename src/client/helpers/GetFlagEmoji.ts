5f9c17535a0d5ad5380bceac11cb6751f509e521
Aryuko <pachimaryu@gmail.com>
2024-02-01 22:29:50 +0100

    Add fallbacks

--- src/client/helpers/GetFlagEmoji.ts
+++ src/client/helpers/GetFlagEmoji.ts
@@ -1,7 +1,12 @@
-export function GetFlagEmoji(countryCode: string): string {
-    const codePoints = countryCode
-        .toUpperCase()
-        .split('')
-        .map(char => 127397 + char.charCodeAt(0))
-    return String.fromCodePoint(...codePoints)
+export function GetFlagEmoji(countryCode?: string): string {
+    if (countryCode) {
+        if (countryCode == "UK") { countryCode = "GB" }
+        const codePoints = countryCode
+            .toUpperCase()
+            .split('')
+            .map(char => 127397 + char.charCodeAt(0))
+        return String.fromCodePoint(...codePoints)
+    } else {
+        return "❓"
+    }
 }
