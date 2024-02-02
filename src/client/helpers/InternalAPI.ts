1714707e6ea20bef025034c8658f4378690763c7
Aryuko <pachimaryu@gmail.com>
2024-02-02 13:51:45 +0100

    Combine fuzzy and exact matches

--- src/client/helpers/InternalAPI.ts
+++ src/client/helpers/InternalAPI.ts
@@ -1,5 +1,10 @@
 const HemaRatingsURL = "/api/hemaratings"
 
+export type FighterSearchResultCombined = {
+    searchTerm: string,
+    matches: Array<FighterResult>
+}
+
 export type FighterSearchResult = {
     searchTerm: string,
     exactMatches: Array<FighterResult>,
@@ -24,7 +29,7 @@ export type RatingResult = {
     rank: number
 }
 
-export function FighterSearchMock(name: string) {
+export function FighterSearchMock(name: string): Promise<FighterSearchResult> {
     return new Promise<any>((resolve, reject) => {
         setTimeout(() => {
             resolve({ "searchTerm": "Einar", "exactMatches": [{ "id": 10369, "name": "Einar Lundgren", "countryCode": "SE", "countryName": "Sweden", "clubId": 64, "clubName": "Stockholmspolisens Idrottsförening Fäktning", "ratings": null }], "fuzzyMatches": [{ "id": 11594, "name": "Einar Schiöth", "countryCode": "IS", "countryName": "Iceland", "clubId": null, "clubName": null, "ratings": null }, { "id": 516, "name": "Einar Hellvik", "countryCode": "NO", "countryName": "Norway", "clubId": 20, "clubName": "Fekteklubben Frie Duellister", "ratings": null }, { "id": 10369, "name": "Einar Lundgren", "countryCode": "SE", "countryName": "Sweden", "clubId": 64, "clubName": "Stockholmspolisens Idrottsförening Fäktning", "ratings": null }, { "id": 3572, "name": "Andreas Reinartz", "countryCode": "DE", "countryName": "Germany", "clubId": 299, "clubName": "Brückenschlag", "ratings": null }, { "id": 3575, "name": "Olga Reinartz", "countryCode": "DE", "countryName": "Germany", "clubId": 299, "clubName": "Brückenschlag", "ratings": null }] })
@@ -32,8 +37,13 @@ export function FighterSearchMock(name: string) {
     })
 }
 
-export function FighterSearch(name: string) {
-    return InternalAPIRequest(`${HemaRatingsURL}/fighters/search/${name}`)
+export function FighterSearch(name: string): Promise<FighterSearchResultCombined> {
+    return InternalAPIRequest(`${HemaRatingsURL}/fighters/search/${name}`).then((JSONResponse: FighterSearchResult) => {
+        return {
+            searchTerm: JSONResponse.searchTerm,
+            matches: JSONResponse.exactMatches.concat(JSONResponse.fuzzyMatches)
+        }
+    })
 }
 
 
