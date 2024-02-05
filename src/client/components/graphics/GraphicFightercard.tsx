ae693fd037e5f7a71bac0ed0d02a57e240ff5ad7
Aryuko <pachimaryu@gmail.com>
2024-02-05 15:07:40 +0100

    Improve GraphicFighterCard css

--- src/client/components/graphics/GraphicFightercard.tsx
+++ src/client/components/graphics/GraphicFightercard.tsx
@@ -6,11 +6,26 @@ function GraphicFightercard(props: FighterResult) {
     let placeholder = "Under construction"
     return (
         <div className="graphic graphic-fightercard">
-            <span>Name: {props.name}</span>
-            <span>Club: {props.clubName}</span>
-            <span>Country: {props.countryName && props.countryCode ? props.countryName + " " + GetFlagEmoji(props.countryCode) : ''}</span>
-            <span>Rank: {placeholder}</span>
-            <span>Wins: {placeholder}</span>
+            <div>
+                <span className='text-grey'>Name</span>
+                <span>{props.name}</span>
+            </div>
+            <div>
+                <span className='text-grey'>Club</span>
+                <span>{props.clubName}</span>
+            </div>
+            <div>
+                <span className='text-grey'>Country</span>
+                <span>{props.countryName && props.countryCode ? props.countryName + " " + GetFlagEmoji(props.countryCode) : ''}</span>
+            </div>
+            <div>
+                <span className='text-grey'>Rank</span>
+                <span>{placeholder}</span>
+            </div>
+            <div>
+                <span className='text-grey'>Wins</span>
+                <span>{placeholder}</span>
+            </div>
         </div>
     )
 }
