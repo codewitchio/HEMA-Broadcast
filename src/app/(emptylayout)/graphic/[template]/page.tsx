29c114a0be7bd3bba13237c10cabd186fbcb2017
Aryuko <pachimaryu@gmail.com>
2024-03-18 00:41:26 +0100

    Add TODO for graphics [data]

--- src/app/(emptylayout)/graphic/[template]/page.tsx
+++ src/app/(emptylayout)/graphic/[template]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import GraphicFightercard from "../../../_components/graphics/GraphicFightercard"
 import { FighterResult } from "../../../_helpers/InternalAPI"
 
+// TODO: Add [data] folder for second param, figure out why JSON parse doesn't work on server
 function GraphicPage({ params }: { params: { template: string, data: string } }) {
     let formattedData: Object = params.data ? JSON.parse(decodeURI(params.data)) : undefined
     let graphicElement: React.ReactElement = <div>Invalid data</div>
