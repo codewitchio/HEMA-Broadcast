bcbac9c56cb2377c96e9b0b9c72c2f36bd08104d
Hanna Kjellén <hanna.kjellen@avanza.se>
2024-04-07 15:01:05 +0200

    Update mock function to include ratings

--- src/app/_helpers/InternalAPI.ts
+++ src/app/_helpers/InternalAPI.ts
@@ -29,10 +29,32 @@ export type RatingResult = {
     rank: number
 }
 
-export function FighterSearchMock(_name: string): Promise<FighterSearchResult> {
+export function FighterSearchMock(_name: string, includeRating: boolean = false): Promise<FighterSearchResult> {
     return new Promise<any>((resolve, _reject) => {
         setTimeout(() => {
-            resolve({ "searchTerm": "Einar", "exactMatches": [{ "id": 10369, "name": "Einar Lundgren", "countryCode": "SE", "countryName": "Sweden", "clubId": 64, "clubName": "Stockholmspolisens Idrottsförening Fäktning", "ratings": null }], "fuzzyMatches": [{ "id": 11594, "name": "Einar Schiöth", "countryCode": "IS", "countryName": "Iceland", "clubId": null, "clubName": null, "ratings": null }, { "id": 516, "name": "Einar Hellvik", "countryCode": "NO", "countryName": "Norway", "clubId": 20, "clubName": "Fekteklubben Frie Duellister", "ratings": null }, { "id": 10369, "name": "Einar Lundgren", "countryCode": "SE", "countryName": "Sweden", "clubId": 64, "clubName": "Stockholmspolisens Idrottsförening Fäktning", "ratings": null }, { "id": 3572, "name": "Andreas Reinartz", "countryCode": "DE", "countryName": "Germany", "clubId": 299, "clubName": "Brückenschlag", "ratings": null }, { "id": 3575, "name": "Olga Reinartz", "countryCode": "DE", "countryName": "Germany", "clubId": 299, "clubName": "Brückenschlag", "ratings": null }] })
+            if (includeRating) {
+                resolve({
+                    "searchTerm": "Einar", "exactMatches": [{
+                        "id": 10369, "name": "Einar Lundgren", "countryCode": "SE", "countryName": "Sweden", "clubId": 64, "clubName": "Stockholmspolisens Idrottsförening Fäktning", "ratings": [{
+                            "ratingCategoryId": 1,
+                            "ratingCategoryName": "Longsword",
+                            "month": "2024-04-07T12:57:10.325Z",
+                            "weightedRating": 1500,
+                            "rank": 10 - 0
+                        }]
+                    }], "fuzzyMatches": [{ "id": 11594, "name": "Einar Schiöth", "countryCode": "IS", "countryName": "Iceland", "clubId": null, "clubName": null, "ratings": null }, { "id": 516, "name": "Einar Hellvik", "countryCode": "NO", "countryName": "Norway", "clubId": 20, "clubName": "Fekteklubben Frie Duellister", "ratings": null }, { "id": 3572, "name": "Andreas Reinartz", "countryCode": "DE", "countryName": "Germany", "clubId": 299, "clubName": "Brückenschlag", "ratings": null }, {
+                        "id": 3575, "name": "Olga Reinartz", "countryCode": "DE", "countryName": "Germany", "clubId": 299, "clubName": "Brückenschlag", "ratings": [{
+                            "ratingCategoryId": 1,
+                            "ratingCategoryName": "Longsword",
+                            "month": "2024-04-07T12:57:10.325Z",
+                            "weightedRating": 1500,
+                            "rank": 10 - 0
+                        }]
+                    }]
+                })
+            } else {
+                resolve({ "searchTerm": "Einar", "exactMatches": [{ "id": 10369, "name": "Einar Lundgren", "countryCode": "SE", "countryName": "Sweden", "clubId": 64, "clubName": "Stockholmspolisens Idrottsförening Fäktning", "ratings": null }], "fuzzyMatches": [{ "id": 11594, "name": "Einar Schiöth", "countryCode": "IS", "countryName": "Iceland", "clubId": null, "clubName": null, "ratings": null }, { "id": 516, "name": "Einar Hellvik", "countryCode": "NO", "countryName": "Norway", "clubId": 20, "clubName": "Fekteklubben Frie Duellister", "ratings": null }, { "id": 10369, "name": "Einar Lundgren", "countryCode": "SE", "countryName": "Sweden", "clubId": 64, "clubName": "Stockholmspolisens Idrottsförening Fäktning", "ratings": null }, { "id": 3572, "name": "Andreas Reinartz", "countryCode": "DE", "countryName": "Germany", "clubId": 299, "clubName": "Brückenschlag", "ratings": null }, { "id": 3575, "name": "Olga Reinartz", "countryCode": "DE", "countryName": "Germany", "clubId": 299, "clubName": "Brückenschlag", "ratings": null }] })
+            }
         }, 100)
     })
 }
