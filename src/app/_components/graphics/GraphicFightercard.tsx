b2449548d9a0412acd90c89e960d3db102f5e70b
Hanna Kjellén <hanna.kjellen@avanza.se>
2024-04-07 16:31:22 +0200

    Streamline props usage

--- src/app/_components/graphics/GraphicFightercard.tsx
+++ src/app/_components/graphics/GraphicFightercard.tsx
@@ -3,34 +3,35 @@ import { GetFlagEmoji } from '../../_helpers/GetFlagEmoji'
 import { FighterResult, RatingResult } from '../../_helpers/InternalAPI'
 import './graphics.css'
 
-export type GraphicFightercardProps = FighterResult & { selectedRating: RatingResult | null } & { isRed: boolean }
+export type GraphicFightercardProps = { fighter: FighterResult, selectedRating: RatingResult | null, isRed: boolean }
 
 function GraphicFightercard(props: GraphicFightercardProps) {
+    const { fighter, selectedRating, isRed } = props
     return (
-        <div className={`card-wrapper vertical-flex ${props.isRed ? ' red' : ''}`}>
+        <div className={`card-wrapper vertical-flex ${isRed ? ' red' : ''}`}>
             <div className='card-border'>
                 <div className='graphic graphic-fightercard noise backdrop'>
                     {/* Fencer name */}
                     <h1 style={{ textAlign: "center", margin: '0' }}>
-                        {props.name || 'Fencer name'} {props.countryCode && GetFlagEmoji(props.countryCode)}
+                        {fighter?.name || 'Fencer name'} {fighter?.countryCode && GetFlagEmoji(fighter?.countryCode)}
                     </h1>
                     {/* Club name */}
                     <div className='card-subtitle horizontal-flex card-secondary-text'>
                         {/* TODO: Fetch short name when length over 35 */}
-                        <span>{props.clubName || 'Fencer club'}</span>
+                        <span>{fighter?.clubName || 'Fencer club'}</span>
                     </div>
                     {/* Rating name */}
                     <div className='card-subtitle horizontal-flex'>
                         <h4>
-                            {props.selectedRating?.ratingCategoryName || 'Select a rating'}
+                            {selectedRating?.ratingCategoryName || 'Rating name'}
                         </h4>
                     </div>
                     {/* Rating and rank */}
                     <div style={{ margin: '0 auto' }}>
                         <span className='card-secondary-text'>Rank: </span>
-                        <span>{props.selectedRating?.rank || 'Select a rating'}</span>
+                        <span>{selectedRating?.rank || '(rank)'}</span>
                         <span className='card-secondary-text'> Rating: </span>
-                        <span>{props.selectedRating?.weightedRating.toFixed(1) || 'Select a rating'}</span>
+                        <span>{selectedRating?.weightedRating.toFixed(1) || '(rating)'}</span>
                     </div>
                 </div>
             </div>
--- src/app/_components/graphics/GraphicLowerThird.tsx
+++ src/app/_components/graphics/GraphicLowerThird.tsx
@@ -4,12 +4,13 @@ import './graphics.css'
 export type GraphicLowerThirdProps = { name: string, subtitle: string, isRed: boolean }
 
 function GraphicLowerThird(props: GraphicLowerThirdProps) {
+    const { name, subtitle, isRed } = props
     return (
-        <div className={`card-wrapper vertical-flex ${props.isRed ? ' red' : ''}`}>
+        <div className={`card-wrapper vertical-flex ${isRed ? ' red' : ''}`}>
             <div className='card-border'>
                 <div className='graphic graphic-lowerthird noise backdrop'>
-                    <span>{props.name || 'Name'}</span>
-                    <span className='card-secondary-text'>{props.subtitle || 'Subtitle'}</span>
+                    <span>{name || 'Name'}</span>
+                    <span className='card-secondary-text'>{subtitle || 'Subtitle'}</span>
                 </div>
             </div>
         </div>
