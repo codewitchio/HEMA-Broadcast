0e8a780966808fca6fa5a2996e87e2bbf5b8f94e
Aryuko <pachimaryu@gmail.com>
2024-04-22 23:50:50 +0200

    Spice up front page

--- app/(defaultlayout)/layout.tsx
+++ app/(defaultlayout)/layout.tsx
@@ -16,16 +16,17 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
         <html lang="en">
             <Script src="https://kit.fontawesome.com/264ffc9fb7.js" crossOrigin="anonymous" />
             <body>
+                <div className="flex flex-row gap-3 text-xl border-b border-border [&>*]:p-4 mb-12">
+                    <Link className="nav-primary" href="/">HEMA Broadcast</Link>
+                    {/* TODO: Graphics dropdown */}
+                    <span className='link text-muted-foreground'>Graphics</span>
+                    <Link className="text-muted-foreground" href="/about">About</Link>
+                </div>
                 <div className="default-layout-root">
-                    <div className="header">
-                        <Link className="nav-primary" href="/">HEMA Broadcast</Link>
-                        <Link className="nav-secondary" href="/about">About</Link>
-                    </div>
                     <div className="content">
                         {children}
                     </div>
-                    <div className="footer">
-                        {/* TODO: Move to about page? */}
+                    <div className="pt-12 pb-4">
                         <div className="frontpage-credits">
                             Author: <a href="https://github.com/Aryuko/">Aryuko</a>, Code: <a href="https://github.com/Aryuko/HEMA-Broadcast">GitHub</a>
                         </div>
@@ -41,6 +42,6 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
                 </svg>
                 <Toaster />
             </body>
-        </html>
+        </html >
     )
 }
--- app/(defaultlayout)/page.tsx
+++ app/(defaultlayout)/page.tsx
@@ -1,14 +1,26 @@
 "use client"
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Page() {
     return (
-        <div className="page page-frontpage vertical-flex">
-            Frontpage :D
-            <Link href="/config/fightercard">Config - Fighter Card</Link>
-            <Link href="/config/lowerthird">Config - Lower Third</Link>
-            {/* Todo: Add list/grid of templates */}
+        <div className="page page-frontpage flex flex-col gap-8 [&>section]:flex [&>section]:flex-col [&>section]:gap-2">
+            <section className='vertical-flex justify-center' style={{ height: "480px" }}>
+                <div className='hero text-center text-6xl'>HEMA-Broadcast</div>
+                <div className='text-center text-xl text-muted-foreground'>An easier way of creating overlays for HEMA events</div>
+            </section>
+            <section>
+                <div className='text-center text-3xl'>Graphic templates</div>
+                <div className='horizontal-flex flex-wrap justify-center graphics-list'>
+                    <Link href="/config/fightercard">Fighter Card</Link>
+                    <Link href="/config/lowerthird">Lower Third</Link>
+                </div>
+            </section>
+            <section>
+                <div className='text-center text-3xl'>Example usage</div>
+                <Image src='/example.png' width={1807} height={1016} alt="example of usage" className='rounded-lg' style={{ boxShadow: "0px 5px 80px 10px var(--card-blue-glow)" }} />
+            </section>
         </div>
     )
 }
