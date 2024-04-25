55171b768d257a0794b9d77fe1d33c05a7235c49
Aryuko <pachimaryu@gmail.com>
2024-04-25 18:19:03 +0200

    Spice up About page significantly

--- app/(defaultlayout)/about/page.tsx
+++ app/(defaultlayout)/about/page.tsx
@@ -1,16 +1,48 @@
 "use client"
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Card, CardContent } from '@/components/ui/card'
+import { GetFlagEmoji } from '@/lib/GetFlagEmoji'
+import GitHubSponsorButton from '@/components/GitHubSponsorButton'
 
 export default function Page() {
     return (
-        <div className="page page-about">
+        <div className="page page-about flex flex-col gap-6">
+            <section className='flex flex-row gap-6 [&>div]:flex [&>div]:flex-col [&>div]:gap-2 [&_h1]:text-2xl [&_h1]:text-center'>
+                <div>
+                    <h1>Author</h1>
+                    <Link href="https://github.com/Aryuko" className='inline-block'>
+                        <Card className='w-[400px] h-30'>
+                            <CardContent className='p-5 pt-5 flex flex-col'>
+                                <div className='flex flex-row gap-2  items-center'>
+                                    <Avatar className='h-12 w-12'>
+                                        <AvatarImage src="https://github.com/aryuko.png" />
+                                        <AvatarFallback>github.com/Aryuko</AvatarFallback>
+                                    </Avatar>
+                                    <div className='flex flex-col gap-1'>
+                                        <p><span className='font-bold'>Hanna</span> <span className='text-muted-foreground text-sm'>(she/they)</span></p>
+                                        <p className='text-muted-foreground text-sm'>github.com/Aryuko</p>
+                                    </div>
+                                </div>
+                                <span className='pt-3 text-muted-foreground text-sm'>{GetFlagEmoji('SE')} Stockholm, Sweden</span>
+                            </CardContent>
+                        </Card>
+                    </Link>
+                </div>
+                <div>
+                    <h1>Repository</h1>
+                    <Link href="https://github.com/Aryuko/HEMA-Broadcast" className='inline-block'>
+                        <Image unoptimized alt='Github Repository preview' width={400} height={120} src={"https://github-readme-stats.vercel.app/api/pin/?username=Aryuko&repo=HEMA-Broadcast&theme=dark&border_color=6a77a4&bg_color=020817&text_color=94a3b8&icon_color=94a3b8&border_radius=10"} />
+                    </Link>
+                </div>
+            </section>
+            <div className='flex justify-center'>
+                <GitHubSponsorButton />
+            </div>
             <p>
-                Project developed by <Link href="https://github.com/Aryuko/">Hanna Kjellén</Link> of <Link href="http://spiffaktning.se/">SPIFF Sweden</Link>.
-                <br />
-                This application is made possible by <Link href="https://hemaratings.com/">HEMA Ratings</Link>.
-                <br />
-                The code for this project is open source and available on <Link href="https://github.com/Aryuko/HEMA-Broadcast">GitHub</Link>.
+                This application was made possible by <Link href="https://hemaratings.com/">HEMA Ratings</Link>.
             </p>
         </div>
     )
--- app/(defaultlayout)/page.tsx
+++ app/(defaultlayout)/page.tsx
@@ -9,11 +9,11 @@ export default function Page() {
     return (
         <div className="page page-frontpage flex flex-col gap-8 [&>section]:flex [&>section]:flex-col [&>section]:gap-2">
             <section className='vertical-flex justify-center h-80'>
-                <div className='hero text-center text-6xl'>HEMA Broadcast Tools</div>
-                <div className='text-center text-xl text-muted-foreground'>An easier way of creating overlays for HEMA events</div>
+                <h1 className='hero text-center text-6xl'>HEMA Broadcast Tools</h1>
+                <span className='text-center text-xl text-muted-foreground'>An easier way of creating overlays for HEMA events</span>
             </section>
             <section>
-                <div className='text-center text-3xl'>Graphic templates</div>
+                <h2 className='text-center text-3xl'>Graphic templates</h2>
                 <div className='horizontal-flex flex-wrap justify-center [&>*]:w-72'>
                     <Card>
                         <CardHeader>
@@ -46,7 +46,7 @@ export default function Page() {
                 </div>
             </section>
             <section>
-                <div className='text-center text-3xl'>Example usage</div>
+                <h2 className='text-center text-3xl'>Example usage</h2>
                 <Image src='/example.png' width={1807} height={1016} alt="example of usage" className='rounded-lg' style={{ boxShadow: "0px 5px 80px 10px var(--card-blue-glow)" }} />
             </section>
         </div>
