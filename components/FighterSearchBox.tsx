7bce7370631759d16bbb94650ce8c866834030c9
Aryuko <pachimaryu@gmail.com>
2024-05-05 20:56:09 +0200

    Fix hydration bug

--- components/FighterSearchBox.tsx
+++ components/FighterSearchBox.tsx
@@ -128,7 +128,7 @@ function FighterSearchBox(props: FighterSearchBoxProps) {
     const showHeader = searchResult && (searchResult.matches.length > 0 || inputValue.length > 0)
 
     return (
-        <form className="fighter-search-box vertical-flex">
+        <div className="fighter-search-box vertical-flex">
             <div className="input-wrapper">
                 <input className={`fighter-search-input ${showHeader ? 'showHeader' : ''}`} type="text" name="name" placeholder="Search for a name" ref={inputRef} value={inputValue} autoComplete="off" onChange={(e) => {
                     setInputValue(e.target.value as string)
@@ -191,7 +191,7 @@ function FighterSearchBox(props: FighterSearchBoxProps) {
                     </SelectTrigger>
                 </Select>
             )}
-        </form>
+        </div>
     )
 }
 
